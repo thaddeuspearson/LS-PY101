330c31b94bf2111ff32b9d3ceaa69916e5ba162a
Thaddeus <thaddeuspearson@gmail.com>
2025-01-06 22:38:47 +0100

    add user input validation funcs

--- Small_Programs/calculator.py
+++ Small_Programs/calculator.py
@@ -1,6 +1,20 @@
+# Constants
+VALID_OPERATIONS = ['1', '2', '3', '4']
+
 # Helper functions
 def prompt(message):
     print(f"==> {message}")
+
+def is_valid_number(num_str):
+    try:
+        int(num_str)
+    except ValueError:
+        prompt("invalid number entered. Try again")
+        return False
+    return True
+
+def is_valid_operation(operation, valid_operations=VALID_OPERATIONS):
+    return True if operation in valid_operations else False
 
 # Print a welcome banner.
 prompt('Welcome to Calculator!')
@@ -9,15 +23,25 @@ prompt('Welcome to Calculator!')
 prompt("What's the first number?")
 number1 = input()
 
+while not is_valid_number(number1):
+    number1 = input()
+
 # Ask the user for the second number.
 prompt("What's the second number?")
 number2 = input()
+
+while not is_valid_number(number2):
+    number2 = input()
 
 # Ask the user for an operation to perform.
 prompt("What operation would you like to perform? \
        \n1) Add 2) Subtract 3) Multiply 4) Divide")
 
 operation = input()
+
+while not is_valid_operation(operation):
+    prompt(f"Please enter one of these numbers: {', '.join(VALID_OPERATIONS)}.")
+    operation = input()
 
 # Perform the operation on the two numbers.
 if operation == "1":
