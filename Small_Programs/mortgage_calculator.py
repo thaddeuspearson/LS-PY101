cc4ea1f01a264995f1d283e967515c04a8272647
Thaddeus <thaddeuspearson@gmail.com>
2025-01-11 00:47:52 +0100

    rm comments from mortgage_calculator for readability

--- Small_Programs/mortgage_calculator.py
+++ Small_Programs/mortgage_calculator.py
@@ -49,32 +49,22 @@ def calculate_monthly_payment(loan_amount, apr, loan_duration):
 
 
 def main():
-    # Load messages from json
     messages = get_message_dict()
-
-    # Print a welcome banner.
     prompt(messages["welcome"])
 
     while True:
-        # Ask the user for the loan amount.
         loan_amount = get_valid_user_input(messages["loan_amount_prompt"],
                                            validation_func=is_valid_number)
 
-        # Ask the user for the annual percentage rate (APR).
         apr = get_valid_user_input(messages["apr_prompt"],
                                    validation_func=is_valid_number)
 
-        # Ask the user for the loan duration (in months)
         loan_duration = get_valid_user_input(messages["loan_duration_prompt"],
                                              validation_func=is_valid_number)
 
-        # Perform the operation on the two numbers.
         result = calculate_monthly_payment(loan_amount, apr, loan_duration)
-
-        # Print the result to the terminal.
         prompt(f"{messages['monthly_payment']}: ${result}")
 
-        # Ask if the user wants to perform another calculation.
         another_op = get_valid_user_input(messages["another_op"],
                                           validation_func=is_valid_operation,
                                           valid_operations=["1", "2"])
