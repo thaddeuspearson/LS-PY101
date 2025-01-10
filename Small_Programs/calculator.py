5266b5a27b3e7f22b05d3fd0dea315b726555726
Thaddeus <thaddeuspearson@gmail.com>
2025-01-11 00:49:07 +0100

    rm comments from calculator for readability

--- Small_Programs/calculator.py
+++ Small_Programs/calculator.py
@@ -54,33 +54,24 @@ def calculate(num_1, num_2, operation):
 
 
 def main():
-    # Load messages from json
     messages = get_message_dict()
 
-    # Print a welcome banner.
     prompt(messages["welcome"])
 
     while True:
-        # Ask the user for the first number.
         num_1 = get_valid_user_input(messages["number_prompt_1"],
                                      validation_func=is_valid_number)
 
-        # Ask the user for the second number.
         num_2 = get_valid_user_input(messages["number_prompt_2"],
                                      validation_func=is_valid_number)
 
-        # Ask the user for an operation to perform.
         operation = get_valid_user_input(messages["operation_prompt"],
                                          validation_func=is_valid_operation,
                                          valid_operations=["1", "2", "3", "4"])
 
-        # Perform the operation on the two numbers.
         result = calculate(num_1, num_2, operation)
-
-        # Print the result to the terminal.
         prompt(f"{messages['result']}: {result}")
 
-        # Ask if the user wants to perform another calculation.
         another_op = get_valid_user_input(messages["another_op"],
                                           validation_func=is_valid_operation,
                                           valid_operations=["1", "2"])
