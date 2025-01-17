c42a7f944d0335095d1f54b57f83299a6959a166
Thaddeus <thaddeuspearson@gmail.com>
2025-01-17 22:43:13 +0100

    add multiplying_two_numbers to Easy_2

---
+++ Small_Problems/Easy_2/multiplying_two_numbers.py
@@ -0,0 +1,29 @@
+"""
+Create a function that takes two arguments, multiplies them together,
+and returns the result.
+"""
+
+
+def multiply(num_1, num_2):
+    """Multiplies two operands together.
+
+    :num_1 (int): the first operand
+    :num_2 (int): the second operand
+    :returns (int): num_1 * num_2
+    """
+    return num_1 * num_2
+
+
+"""CODE EXPLAINATION
+On line 7, the multiply finction is defined with two parameters,
+num_1 and num_2. When executed with two number types passed to
+num_1 and num_2 as arguments, the multiply function returns the
+value of num_1 * num_2.
+"""
+
+
+# Test Cases
+if __name__ == "__main__":
+    assert multiply(5, 3) == 15
+    assert multiply(1, 0) == 0
+    assert multiply(-1, 10) == -10
