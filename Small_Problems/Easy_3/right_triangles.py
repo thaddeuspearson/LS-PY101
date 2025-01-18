282b064d6395d485974d917f5a58891c62031672
Thaddeus <thaddeuspearson@gmail.com>
2025-01-19 00:46:02 +0100

    typo fixes right_triangles.py

--- Small_Problems/Easy_3/right_triangles.py
+++ Small_Problems/Easy_3/right_triangles.py
@@ -6,27 +6,28 @@ of the triangle, and the other end at the upper-right.
 """
 
 
-def triangle(num):
+def triangle(height):
     """Returns a right triangle string composed of spaces and stars.
 
-    :num (int): the int value of the length of the bottom of the right triangle
+    :height (int): the value of the height and the bottom of the right triangle
     :returns triangle_str (str): the formatted triangle string
     """
-    return "\n".join([f"{' '*(int(num) - n)}{'*'*n}" for n in range(1, num+1)])
+    return "\n".join([f"{' '*(int(height) - n)}{'*'*n}"
+                      for n in range(1, height+1)])
 
 
 # pylint: disable=pointless-string-statement
 """CODE EXPLAINATION
-The trianglfe function is defined on line 15, with a single parameter, num.
-When called with a valid int argument passed to num, triangle calls the join
-function on a newline character with an input list comprehension.
+The triangle function is defined on line 15, with a single parameter, height.
+When called with a valid int argument passed to height, triangle calls the
+join function on a newline character with an input list comprehension.
 
-The list comprehension loops from 1 to num + 1, exclusively. At each iteration
-f-string interpolation correctly formates the number of spaces and stars in
-order to print a right triangle with the bottom level equal to the value of
-numstars. Once the loop is finished, and the resulting list comprehension is
-passed to the join method, newlines are inserted in between each formatted
-string and the resulting concatenation is returned.
+The list comprehension loops from 1 to height + 1, exclusively. At each
+iteration f-string interpolation correctly formats the number of spaces and
+stars in order to print a right triangle with the bottom level equal to the
+value of numstars. Once the loop is finished, and the resulting list
+comprehension is passed to the join method, newlines are inserted in between
+each formatted string, and the resulting concatenation is returned.
 """
 
 
