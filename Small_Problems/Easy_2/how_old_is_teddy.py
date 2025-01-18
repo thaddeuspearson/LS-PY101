16cf4f974dbbfd1ab91d9ab815be158c0d7fafd8
Thaddeus <thaddeuspearson@gmail.com>
2025-01-18 01:25:22 +0100

    rf: add name input to how_old_is_teddy

--- Small_Problems/Easy_2/how_old_is_teddy.py
+++ Small_Problems/Easy_2/how_old_is_teddy.py
@@ -2,15 +2,19 @@
 Build a program that randomly generates and prints Teddy's age. To get the age
 you should generate a random number between 20 and 100, inclusive.
 """
+import sys
 from random import randint
 
 
-def teddys_age():
+def teddys_age(name=""):
     """Returns a string that includes Teddy's randomly generatred age
 
+    :name (str): The name of the Teddy bear
     :returns (str): Random age of Teddy string
     """
-    return f"Teddy is {randint(20, 100)} years old!"
+    if not name:
+        name = input("What is the name of your bear? ") or "Teddy"
+    return f"{name} is {randint(20, 100)} years old!"
 
 
 # pylint: disable=pointless-string-statement
@@ -24,8 +28,13 @@ randomly generated age is returned when teddys_age is called.
 
 # Test Cases
 if __name__ == "__main__":
-    AGE_STR = teddys_age()
-    AGE = int(AGE_STR[9:AGE_STR.index(" ", 10)])
-    assert isinstance(AGE_STR, str), "Should return a string"
-    assert (21 <= len(AGE_STR) <= 23), "Should between 21 and 23 chars"
-    assert 20 <= AGE <= 100, "Should be between 20 and 100 inclusively"
+    args = sys.argv[1:]
+    if args and args[0] == "test":
+        TEDDY_STR = teddys_age("Teddy")
+        AGE = int(TEDDY_STR[9:TEDDY_STR.index(" ", 10)])
+        assert isinstance(TEDDY_STR, str), "Should return a string"
+        assert TEDDY_STR[:5] == "Teddy", "Name should be Teddy"
+        assert (21 <= len(TEDDY_STR) <= 23), "Should between 21 and 23 chars"
+        assert 20 <= AGE <= 100, "Age should be between 20 and 100 inclusively"
+    else:
+        print(teddys_age())
