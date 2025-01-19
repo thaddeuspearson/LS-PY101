72bf714d4bee928a890072d46643cbb628078b5a
Thaddeus <thaddeuspearson@gmail.com>
2025-01-20 00:41:14 +0100

    rf: pull conditional logic to a helper function what-century_is_that

--- Small_Problems/Easy_3/what_century_is_that.py
+++ Small_Problems/Easy_3/what_century_is_that.py
@@ -5,42 +5,54 @@ with "st", "nd", "rd", or "th" as appropriate for that number.
 """
 
 
+def century_suffix(cent):
+    """Returns the correct century suffic for the given century.
+
+    :cent (int): the given century
+    :returns cent_suffix (str): the correct suffix for the given century.
+    """
+    if cent % 100 in [11, 12, 13]:
+        return "th"
+    elif cent % 10 == 1:
+        return "st"
+    elif cent % 10 == 2:
+        return "nd"
+    elif cent % 10 == 3:
+        return "rd"
+    else:
+        return "th"
+
+
 def century(year):
     """Returns a formatted string indicating the century of the given year.
 
     :year (int): the given year
-    :returns cent_str (str): the formatted century string
+    :returns (str): the formatted century string
     """
     cent = year // 100 if year % 100 == 0 \
         else (year + 100 - (year % 100)) // 100
 
-    cent_str = str(cent)
-
-    if cent % 100 in range(4, 21):
-        return cent_str + "th"
-    elif cent % 10 == 1:
-        return cent_str + "st"
-    elif cent % 10 == 2:
-        return cent_str + "nd"
-    elif cent % 10 == 3:
-        return cent_str + "rd"
-    else:
-        return cent_str + "th"
+    return f"{cent}{century_suffix(cent)}"
 
 
 # pylint: disable=pointless-string-statement
 """CODE EXPLAINATION
-On line 8, the century function is defined with a single parameter, year.
-On line 14, a ternary statement calculates the given year's century by
+On line 26, the century function is defined with a single parameter, year.
+
+On line 32, a ternary statement calculates the given year's century by
 rounding up to the nearest century using modulo math, and retrieving the
 century number with floor division.
 
-On line 17, the local variable century_str is instantiated with the coerced
-value of century assigned as it's value.
+On line 35, string interpolation concatenates the values of century and its
+corresponding suffix with a call to the century_suffix helper funtion.
 
-The if/elif/else conditional block starting on line 19 checks for specific
+The century_suffix function is defined on line 8, with one parameter, cent.
+The if/elif/else conditional block starting on line 14 checks for specific
 edge cases, and uses modulo math to examine the final digits of century in
-order to concatenate the correct suffix to century_str and return it.
+order to return the correct suffix.
+
+Once the suffix is returned to century's execution context, the final
+formatted century string is returned.
 """
 
 
